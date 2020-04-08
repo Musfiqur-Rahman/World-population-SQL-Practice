6391194a793551d833711e74ab1c0012f22d31c9
MD. Musfiqur Rahman <38560255+Musfiqur-Rahman@users.noreply.github.com>
2020-04-08 16:21:45 +0600

    What is the largest population size for Gabon in this dataset?

---
+++ population_queries.sql
@@ -0,0 +1,8 @@
+-- This is the first query:
+
+SELECT DISTINCT year from population_years;
+
+-- Add your additional queries below:
+
+SELECT country,population FROM population_years
+WHERE country = 'Gabon' ORDER BY population DESC;
